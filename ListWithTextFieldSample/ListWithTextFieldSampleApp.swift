29422e94ff1c7f711c75003ee9850f977e029c62
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-07-09 09:24:24 +0900

    Initial Commit

---
+++ ListWithTextFieldSample/ListWithTextFieldSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListWithTextFieldSampleApp.swift
+//  ListWithTextFieldSample
+//
+//  Created by Yusuke Hasegawa on 2021/07/09.
+//
+
+import SwiftUI
+
+@main
+struct ListWithTextFieldSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
